ef44ae0fe5331f21ed76f51cc0043a98b90bb08d
haosxianr <haosxianr@163.com>
2016-03-12 14:41:04 +0800

    Update Scaner.go

--- Scaner.go
+++ Scaner.go
@@ -1,3 +1,5 @@
+/// Example of a Golang program Syntax Highlighting.
+
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.

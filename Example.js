c1e2b081aa85915c92c7b2cba120a33469c35ac2
haosxianr <haosxianr@163.com>
2016-03-12 21:20:48 +0800

    Create Example.js

---
+++ Example.js
@@ -0,0 +1,53 @@
+/// Example of a JavaScript program Syntax Highlighting.
+
+// ADDING ITEMS TO START AND END OF LIST
+var list = document.getElementsByTagName('ul')[0];                 // Get the <ul> element
+
+// ADD NEW ITEM TO END OF LIST
+var newItemLast = document.createElement('li');                    // Create element
+var newTextLast = document.createTextNode('cream');                // Create text node
+newItemLast.appendChild(newTextLast);                              // Add text node to element
+list.appendChild(newItemLast);                                     // Add element end of list
+
+// ADD NEW ITEM START OF LIST
+var newItemFirst = document.createElement('li');                   // Create element
+var newTextFirst = document.createTextNode('kale');                // Create text node
+newItemFirst.appendChild(newTextFirst);                            // Add text node to element
+list.insertBefore(newItemFirst, list.firstChild);                  // Add element to list
+
+
+
+var listItems = document.querySelectorAll('li');                   // All <li> elements
+
+// ADD A CLASS OF COOL TO ALL LIST ITEMS
+var i;                                                             // Counter variable
+for (i = 0; i < listItems.length; i++) {                           // Loop through elements
+  listItems[i].className = 'cool';                                 // Change class to cool
+}
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading =  headingText + '<span>' + totalItems + '</span>'; // Content
+heading.innerHTML = newHeading;                                    // Update h2 using innerHTML (not textContent) because it contains markup
+
+race = function() {
+  var runners, winner;
+  winner = arguments[0], runners = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+  return print(winner, runners);
+};
+
+if (typeof elvis !== "undefined" && elvis !== null) {
+  alert("I knew it!");
+}
+
+cubes = (function() {
+  var i, len, results;
+  results = [];
+  for (i = 0, len = list.length; i < len; i++) {
+    num = list[i];
+    results.push(math.cube(num));
+  }
+  return results;
+})();

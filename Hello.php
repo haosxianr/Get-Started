853d55c8cd12bbb965a7b5035f03664c1b502600
haosxianr <haosxianr@163.com>
2016-03-12 14:38:19 +0800

    Update Hello.php

--- Hello.php
+++ Hello.php
@@ -1,8 +1,12 @@
+/// Example of a PHP program Syntax Highlighting.
+
+
 <html>
     <head>
         <title>PHP Test</title>
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?> 
+        <?php phpinfo(); ?>
     </body>
 </html>
